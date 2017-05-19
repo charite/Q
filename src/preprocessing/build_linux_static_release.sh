e6f62cc00dc3f62e36ef1da999ccfa02412f6c08
Benjamin Menkuec <benjamin@menkuec.de>
2017-05-19 21:45:44 +0200

    updated binary

--- src/preprocessing/build_linux_static_release.sh
+++ src/preprocessing/build_linux_static_release.sh
@@ -3,7 +3,7 @@ rm -rf preprocessing_build
 mkdir preprocessing_build
 cd preprocessing_build
 export CXX=/usr/bin/g++-5
-cmake -DLINUX_STATIC:BOOL=ON -DCMAKE_BUILD_TYPE=Release -DAVX2:BOOL=OFF ../preprocessing
+cmake -DLINUX_STATIC:BOOL=ON -DCMAKE_BUILD_TYPE=Release -DAVX2:BOOL=ON ../preprocessing
 make flexcat
 make nexcat
 cp -f bin/flexcat ../../bin/
